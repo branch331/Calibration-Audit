393a275fd8674bfe314b7796687c8331bdd0368c
branch331 <34431991+branch331@users.noreply.github.com>
2018-08-18 18:11:37 -0500

    Added 2010 folder
    Added 2010 folder; still needs to be edited for 2010

---
+++ 2012/cs/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace NationalInstruments.Examples.CalibrationAudit
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            worker = new CalibrationAuditWorker();
+            mainGrid.DataContext = worker;
+        }
+
+        private CalibrationAuditWorker worker;
+
+        private void OnRunAuditClick(object sender, RoutedEventArgs e)
+        {
+            worker.StartRunAudit(passwordBox.Password);
+        }
+
+    }
+}
